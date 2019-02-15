f0bc9edb36bb75c4d9a8fda8e24c85253233e0f4
Aliaksandr Yerakhavets <aliaksandr_yerakhave@epbyminw4993.minsk.epam.com>
2019-02-15 20:29:08 +0300

    feature/app/firebase_auth_was_added

--- src/Product/Authentication/Authentication.constants.js
+++ src/Product/Authentication/Authentication.constants.js
@@ -5,5 +5,5 @@ export const EMAIL_KEY = 'EMAIL_KEY';
 export const PASSWORD_KEY = 'PASSWORD_KEY';
 export const UID_KEY = 'UID_KEY';
 
-export const adminUid = 'a3uk0B1GwvSckgaagXPtGo4MAY32';
+export const adminUid = 'jYUtUR6xn9f1lezMBy1pZXTpyc52';
 export const lecturerUid = 'eZZKqqMXmVNqQ7wUacZiVDtAQWK2';
